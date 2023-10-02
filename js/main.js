57e2aa994ff81178aaac8e4820add7fc16eb553e
DanyloSkladan <dskladannnnn09082001@gmail.com>
2023-10-03 02:03:51 +0300

    slideri

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=>{
     menuMobile.classList.toggle('menu--open');
 });
+
+
+const swiperone = new Swiper('.feedback__slider', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
+
+
+  const swipertwo = new Swiper('.sertificates__slider', {
+    
+    loop: true,
+  
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
